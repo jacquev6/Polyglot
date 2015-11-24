afdd6bf8d577ad2db817e0d2f1fd0dccf634ff2f
Vincent Jacques <vincent@vincent-jacques.net>
2015-11-24 16:13:39 +0100

    Move things around

--- go.sh
+++ go.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-for go in $(find . -mindepth 2 -name go.sh)
+for go in */go.sh
 do
     d=$(dirname $go)
     echo $d
     echo $d | sed "s/./-/g"
     cd $d
     ./go.sh
-    cd - >/dev/null
+    cd ..
     echo
 done
