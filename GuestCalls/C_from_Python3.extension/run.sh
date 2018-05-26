31cf18958c82b987543cb83ba787b59f58254a60
Vincent Jacques <vincent@vincent-jacques.net>
2018-05-26 10:42:17 +0200

    Make python scripts not executable

--- GuestCalls/C_from_Python3.extension/run.sh
+++ GuestCalls/C_from_Python3.extension/run.sh
@@ -7,4 +7,4 @@ gcc -fPIC -shared guest.c -o libguest.so
 # This is the manual way to compile the module. This can be abstracted away using 'ext_modules' in setup.py
 gcc -fPIC -shared $(pkg-config python3 --cflags) wrappermodule.c -L. -lguest -o wrapper.so
 
-LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./host.py
+LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH python3 host.py
