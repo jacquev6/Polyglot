6ebb73a76407c0265ba70e6d3a124d5c7ddd42eb
Vincent Jacques <vincent@vincent-jacques.net>
2018-05-26 09:11:30 +0200

    Temporarily restore old examples
    Make sure they run before porting them into GuestCalls

--- old/examples/Python 3.C++.BoostPython.HelloWorld/go.sh
+++ old/examples/Python 3.C++.BoostPython.HelloWorld/go.sh
@@ -2,6 +2,6 @@
 
 set -o errexit
 
-g++ -shared $(pkg-config python3 --cflags) -fPIC hellomodule.cpp -L../../guests/C++ -lhello -lboost_python-py34 -o hello.so
+g++ -shared $(pkg-config python3 --cflags) -fPIC hellomodule.cpp -L../../guests/C++ -lhello -lboost_python-py35 -o hello.so
 
 LD_LIBRARY_PATH=../../guests/C++:$LD_LIBRARY_PATH ./main.py
--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,10 @@
 
 set -o errexit
 
+cd old
+./go.sh
+cd ..
+
 for SCRIPT in $(find . -name run.sh)
 do
     TITLE=$(echo $SCRIPT | sed "s#\./\(.*\)/run\.sh#\1#; s#/#: #g")
