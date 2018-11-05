3beb4ccdb68610d0d695cde878a03d84709bb693
Vincent Jacques <vincent@vincent-jacques.net>
2018-11-05 21:09:57 +0100

    Document how to use Emscripten

--- HelloWorlds/C++_in_JS.Emscripten/run.sh
+++ HelloWorlds/C++_in_JS.Emscripten/run.sh
@@ -2,6 +2,8 @@
 
 set -o errexit
 
+# You probably need to source /path/to/emsdk/emsdk_env.sh to get emcc in your $PATH
+
 em++ hello.cpp -o hello.js
 
 node hello.js
--- HelloWorlds/C_in_JS.Emscripten/run.sh
+++ HelloWorlds/C_in_JS.Emscripten/run.sh
@@ -2,6 +2,8 @@
 
 set -o errexit
 
+# You probably need to source /path/to/emsdk/emsdk_env.sh to get emcc in your $PATH
+
 emcc hello.c -o hello.js
 
 node hello.js
